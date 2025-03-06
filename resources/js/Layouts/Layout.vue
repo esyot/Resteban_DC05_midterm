1b155fec37dee6fba50a9ee847709d49657c930b
Esteban-ExpenseTracker <rei.nhard3367@gmail.com>
2025-03-06 11:26:35 +0800

    first commit

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,56 @@
+<template>
+  <header>
+    <ul>
+      <Link :href="'/'" class="link">
+        <li><span>Home</span></li>
+      </Link>
+      <Link :href="'/cars'" class="link">
+        <li><span>Cars</span></li>
+      </Link>
+      <Link :href="'/bidders'" class="link">
+        <li><span>Bidders</span></li>
+      </Link>
+      <Link :href="'/biddings'" class="link">
+        <li><span>Biddings</span></li>
+      </Link>
+    </ul>
+  </header>
+
+  <slot></slot>
+</template>
+
+<script>
+import { Link } from "@inertiajs/inertia-vue3";
+
+export default {
+  name: "Layout",
+  components: {
+    Link,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  background-color: rgb(82, 157, 255);
+}
+header ul {
+  display: flex;
+  gap: 3rem;
+  list-style-type: none;
+}
+header li {
+  padding: 10px;
+  color: #fff;
+}
+
+header li:hover {
+  background-color: #2461bb;
+  cursor: pointer;
+}
+
+.link {
+  text-decoration: none;
+  color: #000;
+}
+</style>
---
+++ resources/js/Pages/Home/Home.vue
@@ -0,0 +1,16 @@
+<script>
+import Layout from "@/Layouts/Layout.vue";
+export default {
+  layout: Layout,
+  data() {
+    return {
+      message: "Reinahrd Esteban - BSIT-IV",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>Hello World!</h1>
+  <div>{{ message }}</div>
+</template>
